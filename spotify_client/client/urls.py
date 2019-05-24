041ae16d5cc2361c5b30e54d670ea76c4667ec54
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-05-24 00:42:09 -0300

    Update urls adding simple router module

--- spotify_client/client/urls.py
+++ spotify_client/client/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .viewsets import SongsViewSet
+from django.conf.urls import url
+from django.urls import path, include
+from rest_framework import routers
 
+from .viewsets import SongsViewSet
+from .viewsets import UserViewSet
+
+router = routers.SimpleRouter()
+
+router.register(r'songs', SongsViewSet, base_name='songs')
+router.register(r'user', UserViewSet, base_name='user')
 
 urlpatterns = [
-    path('songs/', SongsViewSet.as_view({'get': 'list'}), name="songs-all")
+    url(r'^', include(router.urls)),
 ]
